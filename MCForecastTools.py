34f52655dda2e1ab9599f80db8d1d3007a790562
smruthid <danda.smruthi@gmail.com>
2022-08-08 18:07:22 -0700

    latest changes

--- MCForecastTools.py
+++ MCForecastTools.py
@@ -6,16 +6,21 @@ import datetime as dt
 import pytz
 
 class MCSimulation:
-    def __init__(self, pandas_df):
-        pct_change_df = pandas_df.xs('value',level=1,axis=1).pct_change()
-        cities = pandas_df.columns.get_level_values(0).unique()
-        df1 = pandas_df.sum(level=0, axis=1)
-        df1.columns = pd.MultiIndex.from_product([df1.columns,["pct_change"]])
-        df1 = pd.concat([pandas_df,df1],axis=1).sort_index(1)
-        #for col in pct_change_df.columns:
-            #df1.merge(pct_change_df, how="outer", on="pct_change")
-            #df1[col]["pct_change"] = pct_change_df[col]
-        
-        print(df1)
-        print(pct_change_df)
 
+    def __init__(self, pandas_df, weights=""):
+        pct_change_df = pandas_df.xs('value',level=1,axis=1).pct_change()
+        locations = pandas_df.columns.get_level_values(0).unique()
+        column_names = [(x,"monthly_return") for x in locations]
+        pct_change_df.columns = pd.MultiIndex.from_tuples(column_names)
+        pandas_df = pandas_df.merge(pct_change_df,left_index=True,right_index=True).reindex(columns=locations,level=0)
+        
+        self.df = pandas_df
+        self.weights = weights
+        self.nSim = 500
+        self.nTrading = 12 * 8
+        self.simulated_return = ""
+        
+    def calc_cumulative_return(self):
+        last_prices = self.portfolio_data.xs('value',level=1,axis=1)[-1:].values.tolist()[0]
+        print(last_prices)
+        
