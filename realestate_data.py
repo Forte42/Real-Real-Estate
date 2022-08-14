ed2cb4eb88cc38047919e58874c9fa8c5ac7a2e3
Quinn Wong <quinnwong@Quinns-MBP.attlocal.net>
2022-08-13 17:14:36 -0700

    Moved csv and db files to `data` folder.

--- realestate_data.py
+++ realestate_data.py
@@ -3,31 +3,33 @@ import nasdaqdatalink
 from pathlib import Path
 
 
-def get_regions(regions):    
+def get_regions(regions):
     """
     Fetches a dataframe of Zillow region data (counties, states, etc) from Zillow's REST APIs.
-    
+
     Parameters: 
         regions - the type of region to return, eg county, state 
-        
+
     Returns: 
         DataFrame with Zillow region data
-        
+
     """
-    region_df=nasdaqdatalink.get_table('ZILLOW/REGIONS', region_type=regions)  
+    region_df = nasdaqdatalink.get_table('ZILLOW/REGIONS', region_type=regions)
     return region_df
+
 
 def load_zillow_region_data():
     """
     Fetches Zillow county data and returns a cleaned up DataFrame.
-    
+
     Returns: 
         DataFrame with Zillow county data
-        
+
     """
-        
+
     region_df = get_regions('county')
-    region_df[["county", "state"]] = region_df["region"].str.split(';', 1, expand=True)
+    region_df[["county", "state"]] = region_df["region"].str.split(
+        ';', 1, expand=True)
     region_df["state"] = region_df["state"].str.split(';', 1, expand=True)[0]
 
     #
@@ -39,8 +41,8 @@ def load_zillow_region_data():
     region_df["state"] = region_df['state'].str[1:]
 
     # Clean up region_id datatype.
-    region_df['region_id']=region_df['region_id'].astype(int)
-    
+    region_df['region_id'] = region_df['region_id'].astype(int)
+
     return region_df
 
 
@@ -48,18 +50,19 @@ def load_zillow_sales_data(region_df):
     """
     Loads Zillow sales data from a CSV file.  We then merge the Zillow sales 
     data with the region DataFrame.
-    
+
     Parameters: 
         region_df - Zillow region DataFrame 
-    
+
     Returns: 
         merged DataFrame with Zillow sales and region data.
-        
+
     """
     # A function to load and clean Zillow sales data
     # Reading in Database
     zillow_data = pd.read_csv(
-        Path('ZILLOW_DATA_d5d2ff90eb7172dbde848ea36de12dfe.csv', parse_dates=['date'])
+        Path('./data/ZILLOW_DATA_d5d2ff90eb7172dbde848ea36de12dfe.csv',
+             parse_dates=['date'])
     )
 
     # Merge the Region dataframe with the Zillow sales data
@@ -76,23 +79,25 @@ def get_zillow_data():
     Instructions can be found here https://data.nasdaq.com/databases/ZILLOW/usage/quickstart/python
     Replace 'quandl' w/ 'nasdaqdatalink
     """
-    data = nasdaqdatalink.export_table('ZILLOW/DATA', indicator_id='ZSFH', region_id=list(region_df['region_id']),filename='db.zip')
-    
+    data = nasdaqdatalink.export_table(
+        'ZILLOW/DATA', indicator_id='ZSFH', region_id=list(region_df['region_id']), filename='db.zip')
+
     # Unzipping database from API call
-    shutil.unpack_archive('db.zip')
-    return data        
+    shutil.unpack_archive('./data/db.zip')
+    return data
+
 
 def load_county_coordinates():
     """
     Loads county coordinates data from a CSV file.  
-    
+
     Returns: 
         DataFrame with county coordinates
-        
+
     """
     # Read in county data with coordinates
     county_coordinates_df = pd.read_csv(
-        Path('counties_w_coordinates.csv')
+        Path('./data/counties_w_coordinates.csv')
     )
 
     # Clean up data.
@@ -131,9 +136,9 @@ def load_county_coordinates():
     # Rename column names
     county_coordinates_df.rename(
         columns={'Latitude': 'latitude', 'Longitude': 'longitude'}, inplace=True)
-    
+
     # Drop unnecessary columns
-    county_coordinates_df = county_coordinates_df[['county', 'state', 'latitude', 'longitude']]
+    county_coordinates_df = county_coordinates_df[[
+        'county', 'state', 'latitude', 'longitude']]
 
     return county_coordinates_df
-
